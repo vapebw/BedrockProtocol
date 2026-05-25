f0bfa53d6b676a5d1a2ed6ae0ac14108f003c5d0
vape <jbinact@gmail.com>
2026-05-26 01:13:28 +0200

    Add isset guards in ClientDataToSkinDataHelper to prevent crash with uninitialized fields on legacy clients

--- src/types/login/clientdata/ClientDataToSkinDataHelper.php
+++ src/types/login/clientdata/ClientDataToSkinDataHelper.php
@@ -39,45 +39,74 @@ final class ClientDataToSkinDataHelper{
 	 * @throws \InvalidArgumentException
 	 */
 	public static function fromClientData(ClientData $clientData) : SkinData{
-		/** @var SkinAnimation[] $animations */
 		$animations = [];
-		foreach($clientData->AnimatedImageData as $k => $animation){
+		$animatedImageData = isset($clientData->AnimatedImageData) ? $clientData->AnimatedImageData : [];
+		foreach($animatedImageData as $k => $animation){
 			$animations[] = new SkinAnimation(
 				new SkinImage(
-					$animation->ImageHeight,
-					$animation->ImageWidth,
-					self::safeB64Decode($animation->Image, "AnimatedImageData.$k.Image")
+					isset($animation->ImageHeight) ? $animation->ImageHeight : 0,
+					isset($animation->ImageWidth) ? $animation->ImageWidth : 0,
+					self::safeB64Decode(isset($animation->Image) ? $animation->Image : base64_encode(""), "AnimatedImageData.$k.Image")
 				),
-				$animation->Type,
-				$animation->Frames,
-				$animation->AnimationExpression
+				isset($animation->Type) ? $animation->Type : 0,
+				isset($animation->Frames) ? $animation->Frames : 0.0,
+				isset($animation->AnimationExpression) ? $animation->AnimationExpression : 0
 			);
 		}
+		$skinId = isset($clientData->SkinId) ? $clientData->SkinId : "";
+		$skinResourcePatch = isset($clientData->SkinResourcePatch) ? $clientData->SkinResourcePatch : base64_encode("");
+		$skinImageHeight = isset($clientData->SkinImageHeight) ? $clientData->SkinImageHeight : 0;
+		$skinImageWidth = isset($clientData->SkinImageWidth) ? $clientData->SkinImageWidth : 0;
+		$skinDataStr = isset($clientData->SkinData) ? $clientData->SkinData : base64_encode("");
+		$capeImageHeight = isset($clientData->CapeImageHeight) ? $clientData->CapeImageHeight : 0;
+		$capeImageWidth = isset($clientData->CapeImageWidth) ? $clientData->CapeImageWidth : 0;
+		$capeDataStr = isset($clientData->CapeData) ? $clientData->CapeData : base64_encode("");
+		$skinGeometryData = isset($clientData->SkinGeometryData) ? $clientData->SkinGeometryData : base64_encode("");
+		$skinGeometryDataEngineVersion = isset($clientData->SkinGeometryDataEngineVersion) ? $clientData->SkinGeometryDataEngineVersion : base64_encode("");
+		$skinAnimationData = isset($clientData->SkinAnimationData) ? $clientData->SkinAnimationData : base64_encode("");
+		$capeId = isset($clientData->CapeId) ? $clientData->CapeId : "";
+		$armSize = isset($clientData->ArmSize) ? $clientData->ArmSize : "";
+		$skinColor = isset($clientData->SkinColor) ? $clientData->SkinColor : "";
+		$personaPieces = isset($clientData->PersonaPieces) ? $clientData->PersonaPieces : [];
+		$pieceTintColors = isset($clientData->PieceTintColors) ? $clientData->PieceTintColors : [];
+		$premiumSkin = isset($clientData->PremiumSkin) ? $clientData->PremiumSkin : false;
+		$personaSkin = isset($clientData->PersonaSkin) ? $clientData->PersonaSkin : false;
+		$capeOnClassicSkin = isset($clientData->CapeOnClassicSkin) ? $clientData->CapeOnClassicSkin : false;
+
 		return new SkinData(
-			$clientData->SkinId,
+			$skinId,
 			"",
-			self::safeB64Decode($clientData->SkinResourcePatch, "SkinResourcePatch"),
-			new SkinImage($clientData->SkinImageHeight, $clientData->SkinImageWidth, self::safeB64Decode($clientData->SkinData, "SkinData")),
+			self::safeB64Decode($skinResourcePatch, "SkinResourcePatch"),
+			new SkinImage($skinImageHeight, $skinImageWidth, self::safeB64Decode($skinDataStr, "SkinData")),
 			$animations,
-			new SkinImage($clientData->CapeImageHeight, $clientData->CapeImageWidth, self::safeB64Decode($clientData->CapeData, "CapeData")),
-			self::safeB64Decode($clientData->SkinGeometryData, "SkinGeometryData"),
-			self::safeB64Decode($clientData->SkinGeometryDataEngineVersion, "SkinGeometryDataEngineVersion"), //yes, they actually base64'd the version!
-			self::safeB64Decode($clientData->SkinAnimationData, "SkinAnimationData"),
-			$clientData->CapeId,
+			new SkinImage($capeImageHeight, $capeImageWidth, self::safeB64Decode($capeDataStr, "CapeData")),
+			self::safeB64Decode($skinGeometryData, "SkinGeometryData"),
+			self::safeB64Decode($skinGeometryDataEngineVersion, "SkinGeometryDataEngineVersion"),
+			self::safeB64Decode($skinAnimationData, "SkinAnimationData"),
+			$capeId,
 			null,
-			$clientData->ArmSize,
-			$clientData->SkinColor,
+			$armSize,
+			$skinColor,
 			array_map(function(ClientDataPersonaSkinPiece $piece) : PersonaSkinPiece{
-				return new PersonaSkinPiece($piece->PieceId, $piece->PieceType, $piece->PackId, $piece->IsDefault, $piece->ProductId);
-			}, $clientData->PersonaPieces),
+				return new PersonaSkinPiece(
+					isset($piece->PieceId) ? $piece->PieceId : "",
+					isset($piece->PieceType) ? $piece->PieceType : "",
+					isset($piece->PackId) ? $piece->PackId : "",
+					isset($piece->IsDefault) ? $piece->IsDefault : false,
+					isset($piece->ProductId) ? $piece->ProductId : ""
+				);
+			}, $personaPieces),
 			array_map(function(ClientDataPersonaPieceTintColor $tint) : PersonaPieceTintColor{
-				return new PersonaPieceTintColor($tint->PieceType, $tint->Colors);
-			}, $clientData->PieceTintColors),
+				return new PersonaPieceTintColor(
+					isset($tint->PieceType) ? $tint->PieceType : "",
+					isset($tint->Colors) ? $tint->Colors : []
+				);
+			}, $pieceTintColors),
 			true,
-			$clientData->PremiumSkin,
-			$clientData->PersonaSkin,
-			$clientData->CapeOnClassicSkin,
-			true, //assume this is true? there's no field for it ...
+			$premiumSkin,
+			$personaSkin,
+			$capeOnClassicSkin,
+			true,
 			$clientData->OverrideSkin ?? true,
 		);
 	}
