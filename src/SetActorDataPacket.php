15d0487ed31f04c24f32d42bb4ac03f3f5ebf489
vape <jbinact@gmail.com>
2026-06-10 01:26:11 -0700

    Remove leftover protocol 419 check in SetActorDataPacket

--- src/SetActorDataPacket.php
+++ src/SetActorDataPacket.php
@@ -1,14 +1,6 @@
 <?php
 
-/*
- * This file is part of BedrockProtocol.
- * Copyright (C) 2014-2022 PocketMine Team <https://github.com/pmmp/BedrockProtocol>
- *
- * BedrockProtocol is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- */
+
 
 declare(strict_types=1);
 
@@ -21,23 +13,16 @@ use pocketmine\network\mcpe\protocol\serializer\CommonTypes;
 use pocketmine\network\mcpe\protocol\types\entity\MetadataProperty;
 use pocketmine\network\mcpe\protocol\types\entity\PropertySyncData;
 
-class SetActorDataPacket extends DataPacket implements ClientboundPacket, ServerboundPacket{ //TODO: check why this is serverbound
+class SetActorDataPacket extends DataPacket implements ClientboundPacket, ServerboundPacket{ 
 	public const NETWORK_ID = ProtocolInfo::SET_ACTOR_DATA_PACKET;
 
 	public int $actorRuntimeId;
-	/**
-	 * @var MetadataProperty[]
-	 * @phpstan-var array<int, MetadataProperty>
-	 */
+	
 	public array $metadata;
 	public PropertySyncData $syncedProperties;
 	public int $tick = 0;
 
-	/**
-	 * @generate-create-func
-	 * @param MetadataProperty[] $metadata
-	 * @phpstan-param array<int, MetadataProperty> $metadata
-	 */
+	
 	public static function create(int $actorRuntimeId, array $metadata, PropertySyncData $syncedProperties, int $tick) : self{
 		$result = new self;
 		$result->actorRuntimeId = $actorRuntimeId;
@@ -50,20 +35,14 @@ class SetActorDataPacket extends DataPacket implements ClientboundPacket, Server
 	protected function decodePayload(ByteBufferReader $in) : void{
 		$this->actorRuntimeId = CommonTypes::getActorRuntimeId($in);
 		$this->metadata = CommonTypes::getEntityMetadata($in);
-		if($this->protocolId !== ProtocolInfo::PROTOCOL_1_16_100){
-			$this->syncedProperties = PropertySyncData::read($in);
-		}else{
-			$this->syncedProperties = new PropertySyncData([], []);
-		}
+		$this->syncedProperties = PropertySyncData::read($in);
 		$this->tick = VarInt::readUnsignedLong($in);
 	}
 
 	protected function encodePayload(ByteBufferWriter $out) : void{
 		CommonTypes::putActorRuntimeId($out, $this->actorRuntimeId);
 		CommonTypes::putEntityMetadata($out, $this->metadata);
-		if($this->protocolId !== ProtocolInfo::PROTOCOL_1_16_100){
-			$this->syncedProperties->write($out);
-		}
+		$this->syncedProperties->write($out);
 		VarInt::writeUnsignedLong($out, $this->tick);
 	}
 
